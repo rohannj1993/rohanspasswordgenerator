f894c8b41dacca98bc2c67b9cfa45f2fe0e61475
rohannj1993 <rohannj1993@gmail.com>
2021-05-09 20:40:18 -0400

    final push

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # rohanspasswordgenerator
+We were given starter code to generate a password. While I was able to have prompts appear that allowed me to imput the number of characters that I could use on my project, I could not get the generate password prompt to appear. I made various arrays of all the character options that would be available for the password.
+Also attached are screenshots of the prompts that functioned.
+
+
+![screenshot](assets/screenshots/screenshot16.png)
+![screenshot](assets/screenshots/screenshot17.png)
+![screenshot](assets/screenshots/screenshot18.png)
+![screenshot](assets/screenshots/screenshot19.png)
+![screenshot](assets/screenshots/screenshot20.png)
+![screenshot](assets/screenshots/screenshot21.png)
+![screenshot](assets/screenshots/screenshot22.png)
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,8 +4,9 @@ console.log("first")
 var lowercasecharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "y", "z"]
 var uppercasecharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var specialchar =['!','"','#',$%&'()*+,-./:;<=>?@[\]^_`{|}~]
+var specialchar = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+"]
 
+var possibleChoices= [];
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -19,6 +20,7 @@ function writePassword() {
 }
 
 function generatePassword() {
+  var result = ""
 
   var length = prompt("password needs to be between 8 to 128 characters")
   if (length < 8) {
@@ -27,13 +29,35 @@ function generatePassword() {
   } else if (length > 128) {
     alert("password must be between 8 and 128 characters")
     generatePassword()
-  } else { 
-    var asklowercase = confirm("do you want lower case letter in the password") 
-    var askuppercase =confirm("do you want an upper case letter in the password")
+  } else {
+    var asklowercase = confirm("do you want lower case letter in the password")
+    
+    var askuppercase = confirm("do you want an upper case letter in the password")
+    
     var asknumber = confirm("do you want numbers within password")
+       
     var askspecialchar = confirm("do you want special characters within the password")
+        
+
+ 
+    return result
   }
 
 }
+
+let firstArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10","`" ,"~","!","@","#","$","%","^","&","*","(",")","+"]
+let secondArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","X","Y","Z"]
+let combinedArray1 = firstArray.concat(secondArray)
+let combinedArray2 = secondArray.concat(firstArray)
+
+console.log (combinedArray1)
+// Combined Array1 = 1,2,3,4,5,6,7,8,9,10,`, ~, !,@,#, $,%,^,&,*, (,), +, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, X, Y, Z
+
+console.log(combinedArray2)
+// Combined Array2= a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,X,Y,Z,1,2,3,4,5,6,7,8,9,10,`,~,!,@,#,$,%,^,&,*,(,),+
+
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
